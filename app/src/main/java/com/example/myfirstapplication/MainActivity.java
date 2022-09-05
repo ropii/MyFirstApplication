5b660d16b755a3ac204f136d54035717f130e4d9
Student <roigrad3@gmail.com>
2022-09-05 16:22:51 +0300

    github test

--- app/src/main/java/com/example/myfirstapplication/MainActivity.java
+++ app/src/main/java/com/example/myfirstapplication/MainActivity.java
@@ -3,12 +3,30 @@ package com.example.myfirstapplication;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
+
+    Button btnMain;
+    TextView tvMain;
+    EditText etMain;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        btnMain= findViewById(R.id.btnMain);
+        tvMain= findViewById(R.id.tvMain);
+        etMain= findViewById(R.id.etMain);
+    }
+
+
+    public void onClick(View view) {
+        if (view == btnMain){
+            tvMain.setText(etMain.getText().toString());
+        }
     }
 }
